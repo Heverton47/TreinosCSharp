11ebacb9fc5ba904d492878c1629e35f6d9bfc84
Heverton R.S Santos <hsuchek47@gmail.com>
2026-03-26 09:45:08 -0300

    Update Program.cs

--- aula4/Program.cs
+++ aula4/Program.cs
@@ -1,6 +1,17 @@
-﻿using System;
+using System;
 
 class Program{
+    
+class Produto{
+
+    public string nome;
+    public double preco;
+
+    public void exibir(){
+        Console.WriteLine("Nome: " + nome);
+        Console.WriteLine("Preco:" + preco);
+    }
+}
 static void Main(){
 
     Console.WriteLine("Hello, World!");
